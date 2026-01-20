5748829083c3158e599f7440f77ba46e8165d854
Nikolas Kapralos <160709671+NikolasKapr@users.noreply.github.com>
2026-01-20 16:29:44 +0200

    Update wikipedia.py

--- wikipedia.py
+++ wikipedia.py
@@ -1,17 +1,19 @@
 from .http_session import session
-from .helpers import cleaner
+from .headers import headers
+import requests
 
 def retrieve_wikipedia_description(url):
     id = url.split("=")[-1].strip()
     url = f"https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids={id}&exintro=&exsentences=2&explaintext=&redirects=&formatversion=2&format=json"
-    json_data = requests.get(url, params=params).json()
+    json_data = requests.get(url, headers=headers).json()
     cleaned_first_sents = cleaner(json_data.get('query', {}).get('pages', [{}])[0].get('extract', 'NIL'))
     return cleaned_first_sents
 
 def retrieve_wikipedia_label(url):
     id = url.split("=")[-1].strip()
     url = f"https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids={id}&exintro=&exsentences=2&explaintext=&redirects=&formatversion=2&format=json"
-    json_data = requests.get(url, params=params).json()
+    json_data = requests.get(url, headers=headers).json()
     title = cleaner(json_data.get('query', {}).get('pages', [{}])[0].get('title', 'NIL'))
 
     return title
+
